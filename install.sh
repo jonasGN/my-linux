d4f0ddcab3bf292c504f6ca3a6442b7dc37e2133
jonasGN <j.goncalvesneto@outlook.com>
2022-09-30 17:36:38 -0300

    fix package installation script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ TEMP_WORK_DIR=~/my_linux_temp
 
 # BASIC TOOLS
 BASIC_TOOLS=(
-  "git",
+  "git"
   "curl"
 )
 echo -e $HEADER_COLOR"Instalando pacotes básicos:"$RESET_COLOR
@@ -19,21 +19,32 @@ done
 
 # GNOME INSTALLATION CONFIG
 GNOME_PACKAGES=(
-  "gnome-session",
-  "gnome-shell",
-  "gnome-backgrounds",
-  "gnome-applets",
-  "gnome-control-center",
-  "gnome-tweaks",
-  "mutter",
-  "gjs"
+  "gnome-session"        #
+  "gnome-shell"          # instalado junto com session
+  "gnome-backgrounds"    # instalado junto com session
+  "gnome-applets"        #
+  "gnome-control-center" # instalado junto com session
+  "gnome-tweaks"         #
+  "mutter"               #
+  "gjs"                  # instalado junto com session
 )
 echo -e $HEADER_COLOR"Instalando pacotes gnome:"$RESET_COLOR
 for GNOME_PACKAGE in ${GNOME_PACKAGES[@]}; do
   sudo apt install -y $GNOME_PACKAGE
 done
 
-# theme configuration
+# BASIC APPS CONFIGURATIONS
+echo -e $HEADER_COLOR"Instalando pacotes e aplicações básicas"$RESET_COLOR
+BASIC_APPS=(
+  "gnome-terminal" # terminal padrão
+  "vlc"            # reprodutor de vídeo
+  "firefox-esr"    #  navegador inicial
+)
+for BASIC_APP in ${BASIC_APPS[@]}; do
+  sudo apt install -y $BASIC_APP
+done
+
+# THEME CONFIGURATIONS
 echo -n "Escolha o tema a ser instalado (fluent | orchis) [fluent]: "
 read -t 10 -r GNOME_THEME
 # default theme is `fluent`
