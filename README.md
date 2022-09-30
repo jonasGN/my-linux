f626de2d9c4533739d0682f30c6d9f5a2294957e
jonasGN <j.goncalvesneto@outlook.com>
2022-09-30 13:40:00 -0300

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Linux
+
+Esse repositório contém os scripts para instalação e configuração de Debian de acordo com necessidades pessoais.
+
+
--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# basic tools
+# BASIC TOOLS
 BASIC_TOOLS="git curl"
-
 for TOOL in $BASIC_TOOLS; do
   sudo apt install -y $TOOL
 done
+
+# GNOME INSTALLATION CONFIG
+# GNOME_PACKAGES="gnome-tweaks gnome-session"
+# for GNOME_PACKAGE in $GNOME_PACKAGES; do
+#   sudo apt install -y $GNOME_PACKAGE
+# done
+
+# them configuration
+# echo -n "Escolha o tema a ser instalado (fluent | orchis) [fluent]: "
+# read -t 10 -rs GNOME_THEME
+# # default theme is `fluent`
+# GNOME_THEME="${GNOME_THEME:-fluent}"
+
+TEMP_WORK_DIR=~/my_linux_temp
+
+mkdir $TEMP_WORK_DIR/themes &&
+  cd $TEMP_WORK_DIR/themes &&
+  git clone https://github.com/vinceliuice/Fluent-gtk-theme.git
