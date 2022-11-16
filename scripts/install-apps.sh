0c737ae564b2abc1734b77b127ce549efe4aca14
jonasGN <j.goncalvesneto@outlook.com>
2022-11-16 09:19:41 -0300

    install-apps script updated

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -7,6 +7,11 @@ source config/env.conf
 source helpers/common
 source helpers/prints
 
+# header
+# apps type
+# TYPE="deb" | only is need the name and url to download the deb package from
+# TYPE="script" | apps that already have installation script available from vendor
+
 # export to path to be easy to work with the system files
 export MY_LINUX_DIR=$(pwd)
 
@@ -19,14 +24,31 @@ get_app_type() {
 install_deb_apps() {
   local file="$1"
   if [[ "$(get_app_type $file)" == "deb" ]]; then
-    bash "$file"
+    local appName="$(get_file_name $file).deb"
+    local url=$(grep "URL" $file | cut -d'"' -f 2)
+    local appFile="${MY_LINUX_DIR}/temp/apps/${appName}"
+
+    # download the deb file
+    if ! [[ -f "$appFile" ]]; then
+      print_info "Realizando download do arquivo '$appName'"
+      cd "${MY_LINUX_DIR}/temp/apps"
+      wget -c "$url" -O "$appName" -q --show-progress --progress=bar:force:noscroll
+    else
+      print_info "Arquivo encontrado em '$appFile'"
+      print_info "Pulando etapa de download"
+    fi
+
+    print_info "Executando 'sudo apt install' para '$appName'"
+    echo sudo apt install -y "$appFile"
   fi
 }
 
 install_script_apps() {
   local file="$1"
   if [[ "$(get_app_type $file)" == "script" ]]; then
-    bash "$file"
+    print_info "Encontrado script de instalação para a aplicação"
+    print_info "Executando script de instalação para '$(get_file_name $file)'"
+    echo bash "$file"
   fi
 }
 
