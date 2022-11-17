03075a72cd3f12e782839c9a8b0e35480d52f3ae
jonasGN <j.goncalvesneto@outlook.com>
2022-11-17 14:35:02 -0300

    quick-fix

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 _REPO_URL="https://raw.githubusercontent.com/jonasGN/my-linux/main/install.sh"
 _INSTALLATION_DIR=$(pwd)
 
-echo -e "***** My Linux script de instalação *****"
-echo -e "Para prosseguir, é necessário permissão de super usuário"
+echo "***** My Linux script de instalação *****"
+echo "Para prosseguir, é necessário permissão de super usuário"
 sudo -v
 
-echo -e "Instalando dependências"
+echo "Instalando dependências"
 sudo apt install -y git
 
 git clone "https://github.com/jonasGN/my-linux.git"
 cd "${_INSTALLATION_DIR}/my-linux"
 
-bash "${_INSTALLATION_DIR}/start.sh"
+bash "${_INSTALLATION_DIR}/my-linux/start.sh"
